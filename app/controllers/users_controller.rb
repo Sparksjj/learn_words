f3a5cddca0a47ff1b84e66076c24c850e0ce14d2
Yauhen <Arucard2009@yandex.ru>
2016-02-11 23:36:32 +0300

    ads

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :nead_login,        only:      [:update, :destroy]
-  before_action :correct_user,      only:      [:update]
+  before_action :correct_user,      only:      [:update, :edit]
   before_action :only_admin,        only:      [:destroy]
   before_action :only_not_login,    only:      [:new, :create]
   def index
@@ -57,21 +57,21 @@ class UsersController < ApplicationController
   def nead_login
     unless signed_in?
       store_location
-      flash[:warning]= "You mast login or #{view_context.link_to( 'sign up', new_users_path)} first."  #{link_to 'Sign up', new_users_path} 
+      flash[:warning]= "You mast login or #{view_context.link_to( 'sign up', new_user_path)} first."  #{link_to 'Sign up', new_users_path} 
       redirect_to new_session_path
     end
   end
 
   def correct_user
     unless correct_user?
-      flash[:error]="Sorry bat only admins can do it"
-      redirect_to root_pats
+      flash[:warning]="Maby this page?"
+      redirect_to edit_user_path(current_user)
     end
   end
 
   def only_admin
-    unless current_user.admin?
-      flash[:error]="Thory bat only admins can do it"
+    unless current_user.admin? && signed_in?
+      flash[:errore]="Sorry bat only admins can do it"
       redirect_to :back
     end
   end
